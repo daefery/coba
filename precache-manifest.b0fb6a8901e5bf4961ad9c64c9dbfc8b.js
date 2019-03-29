faf221cd39eaa684ea181ff091a4c524dcee9675
feryyp <feryyundaraputera@gmail.com>
2019-03-29 09:39:09 +0700

    Add files via upload

---
+++ precache-manifest.b0fb6a8901e5bf4961ad9c64c9dbfc8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7cc07fd29aa7e48034d1",
+    "url": "/static/js/main.2fb82803.chunk.js"
+  },
+  {
+    "revision": "bd4b2cd3067abc978823",
+    "url": "/static/js/2.d77dcb7f.chunk.js"
+  },
+  {
+    "revision": "7cc07fd29aa7e48034d1",
+    "url": "/static/css/main.14945911.chunk.css"
+  },
+  {
+    "revision": "98ceefe37477233499a1a11ecf6fedfc",
+    "url": "/index.html"
+  }
+];
